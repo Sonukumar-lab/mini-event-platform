066d910c45316aa6aceb93e0f731c0f495bca8a3
Sonukumar-lab <148353011+Sonukumar-lab@users.noreply.github.com>
2025-12-18 10:38:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
@@ -19,4 +19,6 @@ app.use("/uploads", express.static("uploads"));
 
 
 
-app.listen(process.env.PORT, () => console.log('Server running'));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
